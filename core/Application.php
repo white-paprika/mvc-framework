abc433d92cac6a1669e2c75ffa6a43fa9e424188
Egor Khorunzhiy <e.a.horunjiy@gmail.com>
2024-01-05 13:44:58 +0300

    Add Application, build basic working routing

---
+++ core/Application.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\core;
+
+use app\core\request\Request;
+use app\core\router\Router;
+
+class Application 
+{
+    public $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    public function run()
+    {
+        $this->router->resolve();
+    }
+}
--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
-echo "Hello world!";
+
+use app\core\Application;
+use app\core\request\AppRequest;
+use app\core\router\AppRouter;
+use app\core\StringHelper;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$stringHelper = new StringHelper;
+$request = new AppRequest($stringHelper);
+$router = new AppRouter($request);
+$app = new Application($router);
+
+// Define routes
+$app->router->get('/', function(){
+    echo 'Index page';
+});
+$app->router->get('/posts', function(){
+    echo 'Posts';
+});
+$app->router->post('/posts', function(){
+    echo 'Add post';
+});
+
+$app->run();
