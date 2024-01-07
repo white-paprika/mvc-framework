46d10fa2ae9caf43a927f2f9e38b9dd2ab290b01
Egor Khorunzhiy <e.a.horunjiy@gmail.com>
2024-01-07 17:56:25 +0300

    Add getBetween StringHelper method

--- core/StringHelper.php
+++ core/StringHelper.php
@@ -12,4 +12,13 @@ class StringHelper
         }
         return $string;
     }
+
+    function getBetween($string, $start, $end){
+        $string = ' ' . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return '';
+        $ini += strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+        return substr($string, $ini, $len);
+    }
 }
